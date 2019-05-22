b244ba4f5dbd18546d3cd0c05bd6dc2d746b330e
Lorenzo Stucchi <lorenzo.stucchi@mail.polimi.it>
2019-05-22 09:13:48 +0200

    Created map with leaflet

--- code/webpage.py
+++ code/webpage.py
@@ -4,9 +4,11 @@ from flask import (
 from werkzeug.exceptions import abort
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from psycopg2 import (
-        connect
-)
+from psycopg2 import connect
+
+import pandas as pd
+from sqlalchemy import create_engine
+import geojson
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
@@ -45,6 +47,35 @@ def load_logged_in_user():
         return False
     else: 
         return True
+    
+# block used to download the data of stations from the server
+#def data2geojson(df):
+#    features = []
+#    insert_features = lambda X: features.append(
+#            geojson.Feature(geometry=geojson.Point((X["Longitude"],
+#                                                    X["Latitude"])),
+#                            properties=dict(ID=X["ID"],
+#                                            STALLI=X["STALLI"],
+#                                            BIKE_SH=X["BIKE_SH"],
+#                                            INDIRIZZO=X["INDIRIZZO"],
+#                                            LOCALIZ=X["LOCALIZ"])))
+#    df.apply(insert_features, axis=1)
+#    with open('static/bike_stalls.geojson', 'w', encoding='utf8') as fp:
+#        geojson.dump(geojson.FeatureCollection(features), fp, sort_keys=True, ensure_ascii=False)
+#
+#def data_to_geojson():
+#    myFile = open('dbConfig.txt')
+#    connStr = myFile.readline()
+#    data_conn = connStr.split(" ",2)
+#    dbname = data_conn[0].split("=",1)[1]
+#    username = data_conn[1].split("=",1)[1]
+#    password = data_conn[2].split("=",1)[1]
+#    db_url = 'postgresql://'+username+':'+password+'@localhost:5432/'+dbname
+#    engine = create_engine(db_url)
+#    data = pd.read_sql_table('stations',engine)
+#    data2geojson(data)
+    
+    
         
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -55,6 +86,7 @@ def index():
     cur.close()
     conn.commit()
     load_logged_in_user()
+    #data_to_geojson()
 
     return render_template('index.html')
         
