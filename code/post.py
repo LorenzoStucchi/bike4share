e0a9449cc0dee390359153ad23756d9a36b108c3
FedericaVaghi <FedericaVaghi@users.noreply.github.com>
2019-05-31 13:06:50 +0200

    code implementation
    Forgot password implementation in post script in order to avoid problem with the main. it'll be integrated into the main

--- code/post.py
+++ code/post.py
@@ -4,27 +4,66 @@ Created on Wed May 29 19:38:41 2019
 
 @author: Federica Vaghi
 """
-def post():
+    
+    #FORGOT PASSWORD: it allows to recovery the forgotten password
+@app.route('/ForgotPassword', methods=('GET', 'POST'))
+def ForgotPassword():
+    if request.method == 'POST':
+       user_mail= request.form['user_mail']
+       error = None
+       
+    if not user_mail:
+           error = 'E-mail is required for the Password Recovery'
+            
+    else :
+            conn = get_dbConn()
+            cur = conn.cursor()
+            cur.execute(
+            'SELECT user_mail FROM user_bike WHERE user_mail = %s', (user_mail,))
+            if    cur.fetchone() is not None:
+                  sendmail=cur.fetchone()
+                  cur.close()
+                  conn.commit()
+                  
+            elif  cur.fetchone() is not None:
+                  error = 'E-mail {} is not present, please check it or make the registration procedure again'.format(user_mail)
+                  cur.close()
+                  flash(error)
+                  return render_template('auth/ForgotPassword.html')
+    if error is None:
+            post(sendmail)
+#function that allow to send an email, it'll take the address of the users automatically
+def post():  
     import smtplib
-    receiver='fede94chicca@hotmail.it'
-#   reciever= session.get('user_mail')  #l'idea è di prendere l'indirizzo e-mail dal database
-#    if receiver is None:
-#        g.mail = None
-#    else:
-#        conn = get_dbConn()
-#        cur = conn.cursor()
-#        cur.execute(
-#            'SELECT * FROM user_bike WHERE user_password = %s', (user_password,)
-#        )
-#        g.mail = cur.fetchone()
-#        cur.close()
-#        conn.commit()
-    object= "Subject:Password Recovering!\n\n"
-    content="Here you can find the new password"
+#    if request.method == 'POST':
+#        user_mail = request.form['user_mail']
+#        password = request.form['password']
+#    conn = get_dbConn()
+#    cur = conn.cursor()
+#    cur.execute(
+#                'SELECT password FROM user_bike WHERE user_mail = %s', (password,user_mail,))             
+#    cur.close()
+#    conn.commit()
+    send_mail_to='sendmail'
+    object= "Subject:bike4share Password Recovery!\n\n"
+    content="Here you can find your password:"
     message= object + content
     email = smtplib.SMTP("smtp.gmail.com",587)
     email.ehlo()
     email.starttls()
     email.login("bike4sharepolimi@gmail.com","safelollobike4share")
-    email.sendmail("bike4sharepolimi@gmail.com",receiver,message)
+    email.sendmail("bike4sharepolimi@gmail.com",send_mail_to, message)
     email.quit()
+#POSTINO FUNZIONANTE    
+#    def post():
+#    import smtplib
+#    send_mail_to='fede94chicca@hotmail.it'
+#    object= "Subject:Password Recovering!\n\n"
+#    content="Here you can find the new password:"
+#    message= object + content
+#    email = smtplib.SMTP("smtp.gmail.com",587)
+#    email.ehlo()
+#    email.starttls()
+#    email.login("bike4sharepolimi@gmail.com","safelollobike4share")
+#    email.sendmail("bike4sharepolimi@gmail.com",send_mail_to,message)
+#    email.quit()
