9016f4dd1f9ad1cd27ee13580b9ee6442c702393
Lorenzo Stucchi <lorenzo.stucchi@mail.polimi.it>
2019-05-31 17:46:45 +0200

    Add geolocalization and change icon

--- code/static/map.js
+++ code/static/map.js
@@ -1,13 +1,33 @@
-var mymap = L.map('map').setView([45.47, 9.19], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var layer_mapbox_streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
     ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoibG9yZW56b3N0dWNjaGkiLCJhIjoiY2poOHkxbDV3MDZ6YjMwbzM2M2R1MjYxeiJ9._wsXJw4kbG-02Bhh9EXNQg'
-}).addTo(mymap);
+});
+
+var layer_mapbox_satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
+    ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoibG9yZW56b3N0dWNjaGkiLCJhIjoiY2poOHkxbDV3MDZ6YjMwbzM2M2R1MjYxeiJ9._wsXJw4kbG-02Bhh9EXNQg'
+});
+
+var layer_OSMStandard = L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
+    ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+});
+
+var bike_ic = new L.icon({
+    iconUrl: 'static/bike.png',
+    iconSize:     [50, 50], 
+    iconAnchor:   [25, 50], 
+    popupAnchor:  [0, -53]
+});
 
 function onEachFeature(feature, layer) {
     var popupContent =                        
@@ -18,6 +38,35 @@ function onEachFeature(feature, layer) {
     layer.bindPopup(popupContent)
 }
 
-L.geoJSON(stations,{
+var stat = L.geoJSON(stations,{
+    pointToLayer: function(feature,latlng){
+        return L.marker(latlng,{icon: bike_ic})
+    },
     onEachFeature: onEachFeature
+});
+
+var mymap = L.map('map',{
+    center: [45.47, 9.19],
+    zoom: 13,
+    minZoom: 12,
+    layers: [layer_mapbox_streets, stat]
+});
+
+var baseLayers = {
+    "OpenStreetMap": layer_OSMStandard,
+    "Streets": layer_mapbox_streets,
+    "Satellite": layer_mapbox_satellite
+};
+
+var overlays = {
+    "stations": stat
+};
+
+L.control.layers(baseLayers, overlays).addTo(mymap);
+
+L.Control.geocoder({
+    collapsed: true,
+    position: 'topleft',
+    text: 'Search',
+    title: 'Testing'
 }).addTo(mymap);
--- code/static/map_premium.js
+++ code/static/map_premium.js
@@ -1,13 +1,33 @@
-var mymap = L.map('map_premium').setView([45.47, 9.19], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var layer_mapbox_streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
     ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoibG9yZW56b3N0dWNjaGkiLCJhIjoiY2poOHkxbDV3MDZ6YjMwbzM2M2R1MjYxeiJ9._wsXJw4kbG-02Bhh9EXNQg'
-}).addTo(mymap);
+});
+
+var layer_mapbox_satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
+    ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoibG9yZW56b3N0dWNjaGkiLCJhIjoiY2poOHkxbDV3MDZ6YjMwbzM2M2R1MjYxeiJ9._wsXJw4kbG-02Bhh9EXNQg'
+});
+
+var layer_OSMStandard = L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,' +
+    ' <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+});
+
+var bike_ic = new L.icon({
+    iconUrl: 'static/bike.png',
+    iconSize:     [50, 50], 
+    iconAnchor:   [25, 50], 
+    popupAnchor:  [0, -53]
+});
 
 function onEachFeature(feature, layer) {
     if (feature.properties.FREE == 999)
@@ -32,8 +52,37 @@ function onEachFeature(feature, layer) {
     layer.bindPopup(popupContent)
 }
 
-L.geoJSON(stalls_free,{
+var stat = L.geoJSON(stalls_free,{
+    pointToLayer: function(feature,latlng){
+        return L.marker(latlng,{icon: bike_ic})
+    },
     onEachFeature: onEachFeature
+});
+
+var mymap = L.map('map_premium',{
+    center: [45.47, 9.19],
+    zoom: 13,
+    minZoom: 12,
+    layers: [layer_mapbox_streets, stat]
+});
+
+var baseLayers = {
+    "OpenStreetMap": layer_OSMStandard,
+    "Streets": layer_mapbox_streets,
+    "Satellite": layer_mapbox_satellite
+};
+
+var overlays = {
+    "stations": stat
+};
+
+L.control.layers(baseLayers, overlays).addTo(mymap);
+
+L.Control.geocoder({
+    collapsed: true,
+    position: 'topleft',
+    text: 'Search',
+    title: 'Testing'
 }).addTo(mymap);
 
 mymap.locate({setView: true, maxZoom: 16});
