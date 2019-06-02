f199d77a3b1160b09899ea964172973308036c17
FedericaVaghi <FedericaVaghi@users.noreply.github.com>
2019-06-02 23:58:48 +0200

    txt psw removed

--- code/createSchema.py
+++ code/createSchema.py
@@ -91,12 +91,8 @@ s_k.to_csv('secret_key.txt', header=None, index=None, sep='\n')
 
 p_r = pd.DataFrame({"psw"})
 rec_key = psw_rec_generator()
-cur.execute('INSERT INTO password_recovery (psw_recovery) VALUES (%s)', (rec_key,))
-#p_r.loc[i] = [rec_key]    
+cur.execute('INSERT INTO password_recovery (psw_recovery) VALUES (%s)', (rec_key,))  
 print('Added secret psw', (rec_key,))
-## Save psw_recovery into a txt file
-p_r.to_csv('psw_recovery.txt', header=None, index=None, sep='\n')
-
 
 # Create engine for import dataframe
 db_url = 'postgresql://'+username+':'+password+'@localhost:5432/'+dbname
